26fb1134ad38aa553457cb6985ca139657720a76
海老原祐輔 <ebiyuu1121@gmail.com>
2018-05-26 21:51:54 +0900

    [add]終了時に間違った問題を表示する

--- quiz.py
+++ quiz.py
@@ -21,6 +21,7 @@ while conti:
     #start quiz
     i=0
     c=0
+    wrong=[]
     while i<l:
         ans=input(data[i][2]+'>')
         if ans==data[i][1]:
@@ -29,12 +30,14 @@ while conti:
             i+=1
         elif ans=='':
             print(data[i][1])
+            wrong.append(i)
             i+=1
         elif ans=='q':
             break
         else:
             print('No!')
-    print('result--')
-    print('{0}/{1}'.format(c,i+1))
-    if input('again?(y/N)')!='y':
+    print('\nresult:{0}/{1}\n'.format(c,i+1))
+    for i in wrong:
+        print(data[i][1],data[i][2])
+    if input('\nagain?(y/N)')!='y':
         conti=False
